e52e87922573657cfca45a0cc134ed9b21883f7f
Yannick Thibos <yannick.thibos@infrabel.be>
2021-07-07 13:55:43 +0200

    Wrote first Rover test
    * Initialisation and rotating right once

---
+++ src/main/java/be/jcrafters/thibos/katas/marsrover/Rover.java
@@ -0,0 +1,7 @@
+package be.jcrafters.thibos.katas.marsrover;
+
+public class Rover {
+	public String executeCommands(String r) {
+		throw new UnsupportedOperationException();
+	}
+}
--- src/test/java/be/jcrafters/thibos/katas/marsrover/RoverTest.java
+++ src/test/java/be/jcrafters/thibos/katas/marsrover/RoverTest.java
@@ -1,5 +1,17 @@
 package be.jcrafters.thibos.katas.marsrover;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class RoverTest {
 
+	@Test
+	void givethisanamelater() {
+		Rover rover = new Rover();
+
+		String result = rover.executeCommands("r");
+
+		assertThat(result).isEqualTo("0:0:E");
+	}
 }
